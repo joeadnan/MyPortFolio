ff12831096ad86eebf206219404e67577f3c6e5a
joeadnan <hasyimjoe@gmail.com>
2026-06-14 01:24:49 -0700

    Feat: Project location

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -54,7 +54,7 @@ const projects: Project[] = [
       "Coffe Senja Web App adalah proyek aplikasi berbasis website yang dirancang untuk meningkatkan pengalaman pelanggan (customer experience) di kedai kopi.",
     thumbnail: "../images/Coffe.png",
     tech: ["React Native", "Node.js", "MongoDB"],
-    demoUrl: "https://coffee-shop-6kxkxequv-joeadnans-projects.vercel.app/",
+    demoUrl: "https://coffee-shop-phi-ecru.vercel.app/",
     sourceUrl: "https://github.com/joeadnan/CoffeeShop",
   },
   // {
